2abaf2947bdc3b71f30e6e286c55ee4eade95f76
Benjamim Januário Paulino <benjanuariosite@gmail.com>
2026-05-15 07:54:51 +0200

    Update trabalhoRoutes.js

--- routes/trabalhoRoutes.js
+++ routes/trabalhoRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const trabalhoController = require("../controllers/trabalhoController");
 
-router.post("/secundario", trabalhoController.gerarSecundario);
-router.post("/tecnico", trabalhoController.gerarTecnico);
-router.post("/universitario", trabalhoController.gerarUniversitario);
+const secundarioController = require("../controllers/secundarioController");
+const tecnicoController = require("../controllers/tecnicoController");
+const universitarioController = require("../controllers/universitarioController");
+
+router.post("/secundario/gerar-prompt", secundarioController.gerarPrompt);
+router.post("/secundario/processar", secundarioController.processarJSON);
+
+router.post("/tecnico/gerar-prompt", tecnicoController.gerarPrompt);
+router.post("/tecnico/processar", tecnicoController.processarJSON);
+
+router.post("/universitario/gerar-prompt", universitarioController.gerarPrompt);
+router.post("/universitario/processar", universitarioController.processarJSON);
 
 module.exports = router;
